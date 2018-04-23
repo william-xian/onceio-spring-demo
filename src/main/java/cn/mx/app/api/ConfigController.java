ec6c95f5cdcb9766cac5422e95d1d0b4c3ec83f5
maxian <Ma@danglaoshi.net>
2018-04-23 11:42:20 +0800

    添加websocket示例

--- src/main/java/cn/mx/app/api/ConfigController.java
+++ src/main/java/cn/mx/app/api/ConfigController.java
@@ -1,29 +1,48 @@
 package cn.mx.app.api;
 
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import cn.mx.app.constant.SysConfig;
 import cn.mx.app.holder.WalletHolder;
 import cn.mx.app.model.entity.Wallet;
+import top.onceio.util.OUtils;
 
 @Controller
 public class ConfigController {
     @Autowired
-    private WalletHolder walletProvider;    
+    private WalletHolder walletHolder;
     
     @RequestMapping("/transfer")
     @ResponseBody
-    Wallet home(@RequestParam("from")Long from,@RequestParam("to")Long to,@RequestParam("v") Integer v) {
-    	walletProvider.transfer(from, to, v);
-		return walletProvider.get(to);
+    Wallet transfer(@RequestParam("from")Long from,@RequestParam("to")Long to,@RequestParam("v") Integer v) {
+    	walletHolder.transfer(from, to, v);
+		return walletHolder.get(to);
     }
+
+    @Autowired
+    private DataSource ds;
     @RequestMapping("/config")
     @ResponseBody
-    String home(@RequestParam("name")String name) {
-    	return "pagesize - " + SysConfig.PAGE_SIZE;
+    String config(@RequestParam("name")String name) throws SQLException {
+    	DatabaseMetaData dmd = ds.getConnection().getMetaData();
+    	Map<String,Object> map = new HashMap<>();
+    	map.put("getDatabaseProductName", dmd.getDatabaseProductName());
+    	map.put("getDatabaseProductVersion", dmd.getDatabaseProductVersion());
+    	map.put("getCatalogSeparator", dmd.getCatalogSeparator());
+    	map.put("getDatabaseMajorVersion", dmd.getDatabaseMajorVersion());
+    	map.put("getDatabaseMinorVersion", dmd.getDatabaseMinorVersion());
+    	map.put("getDriverName", dmd.getDriverName());
+    	map.put("getDriverVersion", dmd.getDriverVersion());
+    	return OUtils.toJSON(map);
     }
 }
